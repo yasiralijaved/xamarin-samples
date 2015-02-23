123b3493a6b7a957b8452da40c12c2d51a4c6438
Yasir Ali <ali.yasir0@gmail.com>
2015-02-23 18:28:57 +0500

    Added: Expensometer
    An Application to record the daily expenses. This is developed using
    Xamarin.Forms with MvvM Bindings.

---
+++ Expensometer/Expensometer.Core/Expensometer.Core/ViewModels/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expensometer.Core.ViewModels
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Expensometer/Expensometer.Core/Expensometer.Core/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expensometer.Core.ViewModels
+{
+    class MainPageViewModel : BaseViewModel
+    {
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set
+            {
+                message = value;
+                OnPropertyChanged(message);
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            Message = "Hello World!";
+        }
+    }
+}
